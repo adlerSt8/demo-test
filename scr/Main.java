783258332caea01d699220f21105dc0131d583a7
Андрей <adlerSt@yandex.ru>
2025-05-28 17:54:47 +0300

    Added the Main class with the code.

---
+++ scr/Main.java
@@ -0,0 +1,11 @@
+package scr;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        String str = "Hello";
+        boolean isEmpty = false;
+
+        System.out.println(a + ", " + str + ", " + ", " + isEmpty);
+    }
+}
